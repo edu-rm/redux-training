d507793575b4900891601e9c50b7b4259275e86c
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-04-24 20:42:41 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -11,10 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="App-link"  href="https://reactjs.org" target="_blank" rel="noopener noreferrer"
         >
           Learn React
         </a>
