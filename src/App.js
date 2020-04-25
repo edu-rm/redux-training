56c4bc7e2b7035cc23dde3e7ad76154b0a16f1bb
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-04-25 15:34:55 -0300

    Redux basic configuration

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import GlobalStyle from './styles/global';
 
 import Routes from './routes';
 import history from './services/history';
+import store from './store';
 
 import Header from './components/Header';
 
 function App() {
   return (
-    <Router history={history}>
-      <Header />
-      <GlobalStyle />
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Header />
+        <GlobalStyle />
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
 
